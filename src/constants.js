e4980b4feb422ec95667840355a28a6e7a2e7b8b
Sai Barath <saibarathr@gmail.com>
2023-10-16 23:17:59 +0530

    added contst data like charts and nav items

---
+++ src/constants.js
@@ -0,0 +1,70 @@
+
+import { ReactComponent as DashboardIcon } from "./icons/dashboardkey.svg";
+import { ReactComponent as ProductIcon } from "./icons/product.svg";
+import { ReactComponent as CustomersIcon } from "./icons/customer.svg";
+import { ReactComponent as IncomeICon } from "./icons/income.svg";
+import { ReactComponent as PromoteIcon } from "./icons/promote.svg";
+import { ReactComponent as HelpIcon } from "./icons/help.svg";
+import { ReactComponent as DollarIcon } from "./icons/dollar.svg";
+import { ReactComponent as BagIcon } from "./icons/bag.svg";
+import { ReactComponent as BalanceIcon } from "./icons/balance.svg";
+
+export const SideNavList = [
+    { name: 'Dashboard', icon: <DashboardIcon className="brightness-0 invert" /> },
+    { name: 'Product', icon: <ProductIcon className="brightness-0 invert" /> },
+    { name: 'Customers', icon: <CustomersIcon className="brightness-0 invert" /> },
+    { name: 'Income', icon: <IncomeICon className="brightness-0 invert" /> },
+    { name: 'Promote', icon: <PromoteIcon className="brightness-0 invert" /> },
+    { name: 'Help', icon: <HelpIcon className="brightness-0 invert" /> },
+]
+
+export const cardData = [
+    {
+        title: "Earning",
+        body: "$198k",
+        description: {
+            growth: "37.8%", period: "this month", isProfit: true, icon: <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 20 20" fill="none">
+                <path d="M10 17L10 5" stroke="#00AC4F" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M4.16667 9.99999L10 4.16666L15.8333 9.99999" stroke="#00AC4F" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+            </svg>
+        },
+        icons: <DollarIcon />,
+    },
+    {
+        title: "Orders",
+        body: "$2.4k",
+        description: {
+            growth: "2%", period: "this month", isProfit: false,
+            icon: <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 20 20" fill="none">
+                <path d="M10 3L10 15" stroke="#D0004B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M15.8333 10L10 15.8333L4.16667 10" stroke="#D0004B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+            </svg>
+        },
+        icons: <BalanceIcon />,
+    },
+    {
+        title: "Balance",
+        body: "$2.4k",
+        description: {
+            growth: "2%", period: "this month", isProfit: false,
+            icon: <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 20 20" fill="none">
+                <path d="M10 3L10 15" stroke="#D0004B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M15.8333 10L10 15.8333L4.16667 10" stroke="#D0004B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+            </svg>
+        },
+        icons: <BalanceIcon />,
+
+    },
+    {
+        title: "Total Sales",
+        body: "$89k",
+        description: {
+            growth: "11%", period: "this week", isProfit: true, icon: <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 20 20" fill="none">
+                <path d="M10 17L10 5" stroke="#00AC4F" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M4.16667 9.99999L10 4.16666L15.8333 9.99999" stroke="#00AC4F" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+            </svg>
+        },
+        icons: <BagIcon />,
+
+    },
+]
