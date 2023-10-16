b7ec716af3c91ce8965a557b42d84d1be6fd0470
Sai Barath <saibarathr@gmail.com>
2023-10-16 23:18:38 +0530

    adde sidenav

---
+++ src/Components/SideNav.jsx
@@ -0,0 +1,62 @@
+import { Box, Heading, Text } from '@chakra-ui/react'
+import { ReactComponent as DashboardIcon } from "../icons/dashboard.svg";
+import { ReactComponent as ArrowIcon } from "../icons/arrow.svg";
+import { ReactComponent as AvatarIcon } from "../icons/profile.svg";
+
+import { SideNavList } from '../constants'
+
+import React, { useState } from 'react'
+
+export default function SideNav() {
+    return (
+        <Box className=' px-1 md:px-2 lg:px-4 bg-[#26195f] h-screen flex flex-col justify-start '>
+            <SideNavHeader />
+            <SideNavItems />
+            <SideNavFooter />
+        </Box>
+    )
+}
+
+const SideNavHeader = () => {
+    return <Box className='flex gap-2  py-6'>
+        <DashboardIcon />
+        <Heading fontSize={'26px'} className='font-semibold  text-[#FFFFFF]' >Dashboard</Heading>
+    </Box>
+}
+
+const SideNavItems = () => {
+
+    const [selected, setSelected] = useState(SideNavList[0].name)
+    const handleClickSideNavItem = (name) => () => setSelected(name)
+
+    return <Box className='flex-grow gap-5 flex flex-col my-5'>
+        {SideNavList.map((navItem, index) =>
+            <Box
+                key={navItem.name}
+                className={`p-2 min-h-[46px] flex gap-2 cursor-pointer rounded-md min-w-[200px] items-center ${selected === navItem.name ? 'bg-[#9197B3]' : ''}`}
+                onClick={handleClickSideNavItem(navItem.name)}>
+                {navItem.icon}
+                <Text className='text-base flex-grow'>{navItem.name}</Text>
+                <ArrowIcon />
+            </Box>
+        )}
+    </Box>
+}
+
+const SideNavFooter = () => {
+    return <Box className='flex p-3 m-2 mb-6 rounded-lg bg-[#9197B3] gap-4 items-center ' >
+        <AvatarIcon />
+        <Box className='flex flex-col flex-grow'>
+            <Text className='font-semibold'>
+                Lokesh
+            </Text>
+            <Text className='text-slate-300'>
+                Engineer
+            </Text>
+        </Box>
+        <ArrowIcon className='brightness-0 invert rotate-90' />
+    </Box>
+
+}
+
+
